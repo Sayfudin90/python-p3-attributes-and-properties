3336c9fcb410559c7b285bc55164403d294ab9ba
Sayfudin90 <Seifdin62@gmail.com>
2025-02-17 14:42:56 +0300

    submitting-assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,36 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
+# lib/dog.py
 class Dog:
-    pass
+    approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", 
+                      "French Bulldog", "Pug", "Pointer"]
+    
+    def __init__(self, name=None, breed=None):
+        # Initialize protected attributes
+        self._name = None
+        self._breed = None
+        
+        # Set properties if provided
+        if name is not None:
+            self.name = name
+        if breed is not None:
+            self.breed = breed
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 0 < len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed in Dog.approved_breeds:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,38 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
+# lib/person.py
 class Person:
-    pass
+    approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", 
+                    "Production", "Legal", "Finance", "Sales", 
+                    "General Management", "Research & Development", 
+                    "Marketing", "Purchasing"]
+    
+    def __init__(self, name=None, job=None):
+        # Initialize protected attributes
+        self._name = None
+        self._job = None
+        
+        # Set properties if provided
+        if name is not None:
+            self.name = name
+        if job is not None:
+            self.job = job
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 0 < len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        if job in Person.approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+    
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
